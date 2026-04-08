44c8f07c58f13f628118e986fc7740e011febc9d
VSL Developer <dev@vsl.local>
2026-04-09 00:56:31 +0700

    fix: change some method to approach, fix the structure of logic

---
+++ BussinessLogicLayer/BussinessLayer.cs
@@ -0,0 +1,19 @@
+﻿using SupermarketSystem.DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SupermarketSystem.DataLayer.DataAccessLayer;
+
+namespace SupermarketSystem.BussinessLogicLayer
+{
+    internal class BussinessLayer
+    {
+        public class UserBUS
+        {
+            DataAccessLayer dal = new DataAccessLayer();
+
+        }
+    }
+}
---
+++ DataLayer/DataAccessLayer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketSystem.DataLayer
+{
+    public class DataAccessLayer
+    {
+
+        string ConnStr = "Data Source=DESKTOP-GDJOGDK\\SQLEXPRESS;Initial Catalog=QuanLyBanHang;Integrated Security=True";
+        SqlConnection conn = null;
+        SqlCommand comm = null;
+        SqlDataAdapter da = null;
+        public DataAccessLayer()
+        {
+            conn = new SqlConnection(ConnStr);
+            comm = conn.CreateCommand();
+        }
+        public DataSet ExecuteQueryDataSet(string strSQL, CommandType ct)
+        {
+            if (conn.State == ConnectionState.Open)
+                conn.Close();
+            conn.Open();
+            comm.CommandText = strSQL;
+            comm.CommandType = ct;
+            da = new SqlDataAdapter(comm);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+        }
+        public bool MyExecuteNonQuery(string strSQL, CommandType ct, ref string error)
+        {
+            bool f = false;
+            if (conn.State == ConnectionState.Open)
+                conn.Close();
+            conn.Open();
+            comm.CommandText = strSQL;
+            comm.CommandType = ct;
+            try
+            {
+                comm.ExecuteNonQuery();
+                f = true;
+            }
+            catch (SqlException ex)
+            {
+                error = ex.Message;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return f;
+        }
+    }
+}
